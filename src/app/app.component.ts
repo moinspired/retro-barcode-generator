fb5727789e3b82631983125420b1b909ee6ec897
Motuma Ayana <motuma.ayana1@gmail.com>
2017-12-05 19:56:44 -0500

    make few changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  colors = ['#3CB371', '#B8860B', '#8B0000'];
-  randomVal = Math.floor(Math.random() * 2);
-  numbers = [1,2,3,4,5]
+  colors = ['#3CB371', '#B8860B', '#8B0000', '#00008B'];
+ 
+  randomizeColors = [this.colors[Math.floor(Math.random() * 3)],
+                    this.colors[Math.floor(Math.random() * 3)],
+                    this.colors[Math.floor(Math.random() * 3)],
+                    this.colors[Math.floor(Math.random() * 3)]]
 }
